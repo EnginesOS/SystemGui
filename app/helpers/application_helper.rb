d8cb45d7edd672708cb3de4aedfd2820e745bcf2
James Vodanovich <james@jvodan.com>
2014-08-17 10:45:18 +1000

    rails way to provide access to api class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  require 'EnginesOSapi.rb'
+   def getEnginesOSAPI  
+     @enginesOS_api = EnginesOSapi.new
+   end
+   
+   def enginesOS_api
+      if @enginesOS_api == nill
+        getEnginesOSAPI 
+      end
+     return @enginesOS_api
+   end
 end
