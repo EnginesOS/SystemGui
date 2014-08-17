d88ee6c5b6773b1aed2a57eb28539f107a3bad15
James Vodanovich <james@jvodan.com>
2014-08-17 10:32:14 +1000

    add require

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   
   before_filter :configure_permitted_parameters, if: :devise_controller?
   
+  require 'EnginesOSapi.rb'
   @enginesOS_api = EnginesOSapi.new
   
   def enginesOS_api
