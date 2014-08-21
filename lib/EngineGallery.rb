ae649c9584e643344f8298fa55362abb471447ed
James Vodanovich <james@jvodan.com>
2014-08-22 08:05:33 +1000

    typo

--- lib/EngineGallery.rb
+++ lib/EngineGallery.rb
@@ -155,7 +155,7 @@ end
   
   protected 
   def get_blueprint_entry(blueprint_id)
-    #blueprints_uri =URI('http://220.233.20.158:3001/json_published_softwares') 
+    #blueprint_uri =URI('http://220.233.20.158:3001/json_published_softwares') 
     blueprint_uri =URI(@gallery_url)
            p blueprint_uri
          Net::HTTP.start(blueprint_uri.host, blueprint_uri.port) do |http|
