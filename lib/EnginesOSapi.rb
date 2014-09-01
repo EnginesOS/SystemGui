8398c00dd7ed6c6ff1bdb02cc4571115dd8ef4e6
James Vodanovich <james@jvodan.com>
2014-09-01 20:50:01 +1000

    use full name space on static methods

--- lib/EnginesOSapi.rb
+++ lib/EnginesOSapi.rb
@@ -94,7 +94,7 @@ class EnginesOSapi
     end
     return ret_val
   end
-  
+   
   def EnginesOSapi.loadManagedService(service_name,docker_api)
      yam_file_name = SysConfig.CidDir + "/services/" + service_name + "/config.yaml"
  
@@ -324,7 +324,7 @@ class EnginesOSapi
   end
 
   def stopService service_name
-    service = getManagedService(service_name)
+    service = EnginesOSapi.getManagedService(service_name)
     if service == nil
       return failed(service_name,"No Such Service","Stop Service")
     end
@@ -336,7 +336,7 @@ class EnginesOSapi
   end
 
   def startService service_name
-    service = getManagedService(service_name)
+    service = EnginesOSapi.getManagedService(service_name)
     if service == nil
       return failed(service_name,"No Such Service","Start Service")
     end
@@ -348,7 +348,7 @@ class EnginesOSapi
   end
 
   def  pauseService service_name
-    service = getManagedService(service_name)
+    service = EnginesOSapi.getManagedService(service_name)
     if service == nil
       return failed(service_name,"No Such Service","Pause Service")
     end
@@ -360,7 +360,7 @@ class EnginesOSapi
   end
 
   def  unpauseService service_name
-    service = getManagedService(service_name) 
+    service = EnginesOSapi.getManagedService(service_name) 
     if service == nil
       return failed(service_name,"No Such Service","Unpause Service")
     end
@@ -372,7 +372,7 @@ class EnginesOSapi
   end
 
   def registerServiceWebSite service_name
-    service = getManagedService(service_name)
+    service = EnginesOSapi.getManagedService(service_name)
     if service == nil
       return failed(service_name,"No Such Service","Register Service Web")
     end
@@ -384,7 +384,7 @@ class EnginesOSapi
   end
 
   def deregisterServiceWebSite service_name
-    service = getManagedService(service_name)
+    service =EnginesOSapi.getManagedService(service_name)
     if service == nil
       return  failed(service_name,"No Such Service","Deregister Service Web")
     end
@@ -396,7 +396,7 @@ class EnginesOSapi
   end
 
   def registerServiceDNS service_name
-    service = getManagedService(service_name,"Register Service DNS")
+    service =EnginesOSapi.getManagedService(service_name,"Register Service DNS")
     if service == nil
       return  failed(service_name,service.last_error,"Register Service DNS")
     end
@@ -408,7 +408,7 @@ class EnginesOSapi
   end
 
   def deregisterServiceDNS service_name
-    service = getManagedService(service_name)
+    service =EnginesOSapi.getManagedService(service_name)
     if service == nil
       return  failed(service_name,service.last_error,"Deregister Service DNS")
     end
@@ -420,7 +420,7 @@ class EnginesOSapi
   end
 
   def createService service_name
-    service = getManagedService(service_name)
+    service =EnginesOSapi.getManagedService(service_name)
     if service == nil
       return  failed(service_name,service.last_error,"Create Service")
     end
@@ -432,7 +432,7 @@ class EnginesOSapi
   end
 
   def recreateService service_name
-    service = getManagedService(service_name)
+    service =EnginesOSapi.getManagedService(service_name)
     if service == nil
       return failed(service_name,"No Such Service","Recreate Service")
     end
