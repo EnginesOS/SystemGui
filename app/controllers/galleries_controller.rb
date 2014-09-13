3a505dfdd7925ccba9c713fb40dbb689a7462b99
Lachlan <lachdoug@gmail.com>
2014-09-14 04:24:51 +1000

    fixed control panel nav items active state

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -4,23 +4,6 @@ class GalleriesController < ApplicationController
 
   def index
     @galleries = Gallery.all
-    @added_gallery_servers = []
-    @galleries.each do |gallery|
-      @added_gallery_servers << gallery.gallery_server
-    end
-    @added_gallery_server_short_names = @added_gallery_servers.map(&:short_name)
-    @all_gallery_servers = EngineGallery.list_local
-    @all_gallery_server_short_names = @all_gallery_servers.map(&:short_name)
-    @unadded_gallery_server_short_names = @all_gallery_server_short_names - @added_gallery_server_short_names
-
-    @unadded_gallery_servers = []
-    @unadded_gallery_server_short_names.each do |unadded_gallery_server_short_name|
-      @all_gallery_servers.each do |gallery_server|
-        if gallery_server.short_name == unadded_gallery_server_short_name
-          @unadded_gallery_servers << gallery_server
-        end
-      end
-    end
   end
 
   def new
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,14 @@ class PagesController < ApplicationController
     render :home, layout: false
   end
 
+  def app_manager
+    @services = @enginesOS_api.getManagedServices()
+    @services ||= []
+
+    @engines = @enginesOS_api.getManagedEngines()
+    @engines ||= []
+  end
+
   def installer
     @galleries = Gallery.all
     @gallery_servers = []
@@ -20,12 +28,25 @@ class PagesController < ApplicationController
     @gallery_servers.flatten!
   end
 
-  def app_manager
-    @services = @enginesOS_api.getManagedServices()
-    @services ||= []
+  def galleries
+    @galleries = Gallery.all
+    @added_gallery_servers = []
+    @galleries.each do |gallery|
+      @added_gallery_servers << gallery.gallery_server
+    end
+    @added_gallery_server_short_names = @added_gallery_servers.map(&:short_name)
+    @all_gallery_servers = EngineGallery.list_local
+    @all_gallery_server_short_names = @all_gallery_servers.map(&:short_name)
+    @unadded_gallery_server_short_names = @all_gallery_server_short_names - @added_gallery_server_short_names
 
-    @engines = @enginesOS_api.getManagedEngines()
-    @engines ||= []
+    @unadded_gallery_servers = []
+    @unadded_gallery_server_short_names.each do |unadded_gallery_server_short_name|
+      @all_gallery_servers.each do |gallery_server|
+        if gallery_server.short_name == unadded_gallery_server_short_name
+          @unadded_gallery_servers << gallery_server
+        end
+      end
+    end
   end
 
   def system
