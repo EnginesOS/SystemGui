e34b1063aad8d44a016befc869a49e3d4d7d6c5c
James Vodanovich <james@jvodan.com>
2014-09-29 15:30:29 +1000

    need for prod

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,8 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.secret_key = '622af3663ba5718719668ed9b39ff868b4b5a92cf5ae472c76d7215b97703f1483269296d5bb25e15a78b30e440464207c497279337e698ec61718d629c9c6d3'
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
