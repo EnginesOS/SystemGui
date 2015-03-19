76470da3406d6d5b1e866ae41f1be05278a2cbf6
Lachlan <lachdoug@gmail.com>
2015-03-19 12:29:13 +1100

    handle error on getting software

--- app/models/concerns/engines_software_collector.rb
+++ app/models/concerns/engines_software_collector.rb
@@ -1,15 +1,35 @@
 module EnginesSoftwareCollector
 
   def all_engine_names
-    engines_api.list_apps
+    engines = engines_api.list_apps
+      if engines.kind_of?(EnginesOSapiResult)
+        []
+      else
+        engines
+      end
+    end
   end
 
   def all_host_names
-    all_engine_names.map { |engine_name| engines_software(engine_name).hostName }
+    all_engine_names.map do |engine_name|
+      engine = engines_software(engine_name)
+      if engine.kind_of?(EnginesOSapiResult)
+        next
+      else
+        engine.hostName
+      end
+    end
   end
 
   def all_fqdns
-    all_engine_names.map { |engine_name| engines_software(engine_name).fqdn }
+    all_engine_names.map do |engine_name|
+      engine = engines_software(engine_name)
+      if engine.kind_of?(EnginesOSapiResult)
+        next
+      else
+        engine.fqdn
+      end
+    end
   end
 
   def count
