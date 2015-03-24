8ef90ac1c4cb44adf4e1dd5edb9ae42d7d6853de
Lachlan <lachdoug@gmail.com>
2015-03-25 08:47:57 +1100

    work on attached services and backups

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
     $('.selectize-single').selectize();
-});
 
-$(document).ready(function() {
     $('.selectize-multi').selectize(
 // selectize needs to be configured for multi-select.
     );
+
+    // $(".software_installation_form .advanced_fields").hide();
+    $("#show_advanced_fields_button").click(function() {
+      $(".new_software_form_default_details").toggle();
+      $(".advanced_fields").toggle();
+    });
+
 });
--- app/controllers/attached_services_controller.rb
+++ app/controllers/attached_services_controller.rb
@@ -4,39 +4,24 @@ class AttachedServicesController < ApplicationController
   
   def index
     @software = Software.find(params[:software_id])
-    # render text: EnginesSoftware.attached_services(@software.engine_name)
     @software.build_attached_services_handler.load_attached_services
   end
 
   def new
     @software = Software.find(params[:software_id])
-    @attached_service = @software.build_attached_services_handler.attached_services.build(new_attached_service_attributes)
-    
+    @attached_services_handler = @software.build_attached_services_handler
+    @attached_service = @attached_services_handler.attached_services.build(new_attached_service_attributes)
   end
 
   def create
     @software = Software.find(params[:software_id])
     @attached_service = @software.build_attached_services_handler.attached_services.build(attached_service_attributes)
-# render text: build_attached_service_attributes
-    # @attached_services_handler = @software.build_attached_services_handler
-# 
-    # @attached_service = @attached_services_handler.attached_services.build(attached_service_attributes)
-#     
-    # @attached_service = 
-    # render :new
-    # render text: @attached_service.variables.first.field_type
-    
-    # @software = Software.find(params[:software_id])
-    # @attached_service = @software.build_attached_services_handler.
-      # attached_services.build(attached_service_attributes)
-      
     if @attached_service.valid?
       result = EnginesAttachedService.attach_service(build_attached_service_attributes)
       if result.was_success == true
-        redirect_to control_panel_path, notice: "Successfully attached #{@attached_service.title} to #{@software.engine_name}."
+        redirect_to attached_services_path(software_id: @software.id), notice: "Successfully attached #{@attached_service.title} to #{@software.engine_name}."
       else
-        flash.now[:alert] = "Failed to attach #{@attached_service.title} to #{@software.engine_name}. #{result.result_mesg}"[0..1000]
-        render :new
+        redirect_to attached_services_path(software_id: @software.id), alert: "Failed to attach #{@attached_service.title} to #{@software.engine_name}. #{result.result_mesg}"[0..1000]
       end
     else
       render :new
@@ -48,7 +33,7 @@ private
   def new_attached_service_attributes
     type_path = params[:attached_service][:type_path]
     publisher_namespace = params[:attached_service][:publisher_namespace]
-    attached_service_service_detail = service_detail type_path, publisher_namespace
+    attached_service_service_detail = @attached_services_handler.service_detail type_path, publisher_namespace
     {
       type_path: type_path,
       publisher_namespace: publisher_namespace,
@@ -59,31 +44,10 @@ private
   end
 
   def attached_service_attributes
-    attached_service_params = params.require(:attached_service).permit(:type_path, :publisher_namespace, :title, :description, variables_attributes: [
-      :name,
-      :value,
-      :value_confirmation,
-      :label,
-      :field_type,
-      :select_collection,
-      :tooltip,
-      :hint,
-      :placeholder,
-      :comment,
-      :regex_validator,
-      :regex_invalid_message,
-      :mandatory,
-      :ask_at_build_time,
-      :build_time_only,
-      :immutable
+    params.require(:attached_service).permit(:type_path, :publisher_namespace, :title, :description, variables_attributes: [
+      :name, :value, :value_confirmation, :label, :field_type, :select_collection, :tooltip, :hint, :placeholder, 
+      :comment, :regex_validator, :regex_invalid_message, :mandatory, :ask_at_build_time, :build_time_only, :immutable
     ])
-    # {
-      # type_path: attached_service_params[:type_path],
-      # publisher_namespace: attached_service_params[:publisher_namespace],
-      # title: attached_service_params[:title],
-      # description: attached_service_params[:description],
-      # variables_attributes: attached_service_params[:variables_attributes].values
-    # }
   end
 
   def build_attached_service_attributes
@@ -93,19 +57,19 @@ private
       publisher_namespace: @attached_service.publisher_namespace,
       title: @attached_service.title,
       variables: {}
-      }
+    }
     @attached_service.variables.each do |variable|
       result[:variables][variable.name.to_sym] = variable.value
     end
     return result
   end
 
-  def service_detail(type_path, publisher_namespace)
-    @software.attached_services_handler.available_services.find do |service|
-      service[:type_path] == type_path &&
-      service[:publisher_namespace] == publisher_namespace
-    end
-  end
+  # def service_detail(type_path, publisher_namespace)
+    # @software.attached_services_handler.available_services.find do |service|
+      # service[:type_path] == type_path &&
+      # service[:publisher_namespace] == publisher_namespace
+    # end
+  # end
 
   # def new_attached_service_variable_attributes setup_params
     # variables_params = setup_params.values
