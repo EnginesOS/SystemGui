70d83941e0a656183e253a33c5bd85e79943496c
Lachlan <lachdoug@gmail.com>
2015-07-17 19:37:19 +1000

    restart and more

--- app/assets/javascripts/restarting.js
+++ app/assets/javascripts/restarting.js
@@ -1,16 +1,30 @@
 $(document).ready(function(){
 
 	if ($("#system_restarting").length > 0) {
-		restarting_poll();
+		restarting_poll_progress();
+		do_restarting_poll();
+	};
+
+	function do_restarting_poll() {
+	   	setTimeout(function() {
+	       	restarting_poll();
+	    }, 10000);
 	};
 
 	function restarting_poll() {
-		$("#restarting_timer_message").append(".");
 	   	setTimeout(function() {
 	       	$.ajax({ url: "/system/restarting", success: function(data) {
 	        	window.location.href = "/control_panel";
 	       }, complete: restarting_poll });
-	    }, 5000);
+	    }, 2000);
+	};
+
+	function restarting_poll_progress() {
+	   	setTimeout(function() {
+	       	$.ajax({ url: "/system/restarting", success: function(data) {
+				$("#restarting_message").append(".");
+	       }, complete: restarting_poll_progress });
+	    }, 500);
 	};
 
 });
--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -6,16 +6,16 @@ class SystemsController < ApplicationController
   end
   
   def restart
-    # result = System.restart
-    # if result.kind_of?(EnginesOSapiResult)
-      # if result.was_success
+    result = System.restart
+    if result.kind_of?(EnginesOSapiResult)
+      if result.was_success
         redirect_to restarting_system_path
-      # else
-        # redirect_to control_panel_path, alert: ( "Unable to restart system. " + result.result_mesg )[0,500]
-      # end
-    # else
-      # redirect_to control_panel_path, alert: "No result"
-    # end
+      else
+        redirect_to control_panel_path, alert: ( "Unable to restart system. " + result.result_mesg )[0,500]
+      end
+    else
+      redirect_to control_panel_path, alert: "No result"
+    end
   end
   
   def restarting
