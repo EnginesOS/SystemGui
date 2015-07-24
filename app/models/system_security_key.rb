b4758f56451afeca89572957c0f32741634abe38
Lachlan <lachdoug@gmail.com>
2015-07-24 15:21:27 +1000

    more work on security

--- app/models/system_security_key.rb
+++ app/models/system_security_key.rb
@@ -1,8 +1,21 @@
 class SystemSecurityKey < ActiveRecord::Base
 
-  has_attached_file :public_key
+  include Engines::Api
 
-  attr_accessor :key
+  has_attached_file :public_key
+  validates_attachment_content_type :public_key, :content_type => /\Atext\/.*\Z/
+  
+  def save
+    valid? && persist!
+  end
+
+  def persist!
+    engines_api.update_public_key(file_content).was_success
+  end
+
+  def file_content
+    open(self.public_key.queued_for_write[:original].path).read
+  end
 
 end
 
