944c4ee267623cb1b9c10db16326a55ef8d7522c
Lachlan <lachdoug@gmail.com>
2015-09-10 12:33:39 +1000

    better handling of service errors

--- app/models/engines/service.rb
+++ app/models/engines/service.rb
@@ -6,12 +6,13 @@ module Engines::Service
   
   def system_service_object
     @system_service_object ||= engines_api.getManagedService container_name
+    @system_service_object = nil if @system_service_object.is_a? EnginesOSapiResult
   end
 
 #extractors
   
   def human_name
-    titles_data[container_name.to_sym][:title]
+    titles_data[container_name.to_sym].present? ? titles_data[container_name.to_sym][:title] : 'missing'
   end
 
   def fa_icon
@@ -35,8 +36,7 @@ module Engines::Service
   end
 
   def state
-    result = system_service_object.read_state
-    if result == 'nocontainer'
+    if system_service_object.blank? || (system_service_object.read_state == 'nocontainer')
       'no_container'
     else
       result
@@ -54,91 +54,100 @@ module Engines::Service
   # end
   
   def web_sites
-    service_fqdn = system_service_object.web_sites
+    system_service_object.blank? ? [] : system_service_object.web_sites
   end
 
   def default_startup_state
-    system_service_object.setState
+    system_service_object.blank? ? '?' : system_service_object.setState
   end
 
   def memory
-    system_service_object.memory
-  end
-
-  def framework
-    system_service_object.framework
-  end
-
-  def runtime
-    system_service_object.runtime
-  end
-
-  def image
-    system_service_object.image
-  end
-
-  def repository
-    system_service_object.repository
-  end
-
-  def port
-    system_service_object.web_port
-  end
-
-  def eports
-    system_service_object.eports
-  end
-
-  def last_error
-    system_service_object.last_error
+    system_service_object.blank? ? '?' : system_service_object.memory
   end
 
   def is_error?
-    system_service_object.is_error?
+    system_service_object.blank? ? true : system_service_object.is_error?
+  end
+
+  def framework
+    system_service_object.blank? ? '?' : system_service_object.framework
+  end
+
+  def runtime
+    system_service_object.blank? ? '?' : system_service_object.runtime
+  end
+
+  def image
+    system_service_object.blank? ? '?' : system_service_object.image
+  end
+
+  def repository
+    system_service_object.blank? ? '?' : system_service_object.repository
+  end
+
+  def port
+    system_service_object.blank? ? '?' : system_service_object.web_port
+  end
+
+  def eports
+    system_service_object.blank? ? [] : system_service_object.eports
+  end
+
+  def last_error
+    system_service_object.blank? ? '?' : system_service_object.last_error
   end
 
   def last_result
-    system_service_object.last_result
+    system_service_object.blank? ? '?' : system_service_object.last_result
   end
 
   def environments
-    system_service_object.environments
+    system_service_object.blank? ? nil : system_service_object.environments
   end
 
   def volumes
-    system_service_object.volumes
+    system_service_object.blank? ? [] : system_service_object.volumes
   end
 
   def consumers
-    system_service_object.registered_consumers
+    system_service_object.blank? ? [] : system_service_object.registered_consumers
   end
 
   def stats
-    system_service_object.stats
+    system_service_object.blank? ? nil : system_service_object.stats
   end
 
   def ps_container
-    system_service_object.ps_container
+    system_service_object.blank? ? nil : system_service_object.ps_container
   end
 
   def logs_container
-    system_service_object.logs_container
+    system_service_object.blank? ? nil : system_service_object.logs_container
   end
 
+
+
+
+
   def network_metrics
-    engines_api.get_container_network_metrics container_name
+    # system_service_object.blank? ? '?' : system_service_object.memory
+    result = engines_api.get_container_network_metrics container_name
+    return {} unless result.is_a? Hash
+    result
   end
 
   def memory_statistics
-    engines_api.get_service_memory_statistics container_name
+    result = engines_api.get_service_memory_statistics container_name
+    return {} unless result.is_a? Hash
+    result
   end
   
   def type_path
-    system_service_object.type_path
+    system_service_object.blank? ? nil : system_service_object.type_path
   end
   
   def publisher_namespace
-    system_service_object.publisher_namespace
+    system_service_object.blank? ? nil : system_service_object.publisher_namespace
   end
 
   def service_definition
@@ -233,7 +242,8 @@ private
       mongo_server: {title: 'Mongo NoSQL database', fa_icon: 'database'},
       mysql_server: {title: 'MySQL database', fa_icon: 'database'},
       nginx: {title: 'Web router', fa_icon: 'random'},
-      cAdvisor: {title: 'Activitiy monitor', fa_icon: 'bar-chart'},
+      cAdvisor: {title: 'Activitiy monitor', fa_icon: 'area-chart'},
+      wwwstats: {title: 'Web statistics', fa_icon: 'bar-chart'},
       pgsql_server: {title: 'Postgres database', fa_icon: 'database'},
       smtp: {title: 'Outbound mail', fa_icon: 'send-o'},
       volmanager: {title: 'File system manager', fa_icon: 'folder-o'},
