db536199ef6bcf50ebe1130249ca3495e963332f
Lachlan Douglas <lachlan@engines.org>
2016-01-08 16:44:05 +1100

    rename methods and remove debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -60,7 +60,7 @@ protected
       'service_reports',
       'application_abouts',
       'service_abouts',
-      'gallery_softwares',
+      'library_softwares',
       'system_monitor_charts',
       'control_panel_applications_states',
       'control_panel_services_states'
@@ -159,6 +159,12 @@ protected
   end
 
   def render_500(exception)
+
+# ENV['SEND_BUG_REPORTS'] = false.to_s
+p :send_bug_reports_check
+p ENV['SEND_BUG_REPORTS']
+
+
     SystemUtils.log_exception exception
     @exception = exception
     render 'systems/500', :status => 500, layout: 'empty_navbar'
