a2b3e462377723188825c29545cac745f378e963
Lachlan Douglas <lachlan@engines.org>
2016-01-08 17:20:13 +1100

    hide mesg on success

--- app/controllers/system_restart_mgmts_controller.rb
+++ app/controllers/system_restart_mgmts_controller.rb
@@ -6,7 +6,7 @@ class SystemRestartMgmtsController < ApplicationController
       if result.kind_of?(EnginesOSapiResult)
         if result.was_success
           @system_status = System.status
-          flash[:notice] = result.result_mesg
+          # flash[:notice] = result.result_mesg
         else
           redirect_to services_control_panel_path, alert: ( "Unable to restart Engines system manager. " + result.result_mesg )[0,500]
         end
