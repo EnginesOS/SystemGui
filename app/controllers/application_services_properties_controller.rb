7add791a07101155787b409210542d3d7e9527cb
Lachlan Douglas <lachlan@engines.org>
2016-02-25 12:36:36 +1100

    remove debug

--- app/controllers/application_services_properties_controller.rb
+++ app/controllers/application_services_properties_controller.rb
@@ -3,14 +3,8 @@ class ApplicationServicesPropertiesController < ApplicationController
 include Engines::Api
 
   def show
-    
-    
     @application_services_properties = Application.find_by(container_name: application_name).build_application_services_properties
     @application_services_properties.build_application_services
-
-    @x = engines_api.list_attached_services_for('ManagedEngine', @application_services_properties.application.container_name)
-
-
   end
 
 private
