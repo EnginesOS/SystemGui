a2a81f33414fa9e174b136bce722d829851b5bd0
Lachlan Douglas <lachlan@engines.org>
2016-03-17 15:58:37 +1100

    changes to service connection params

--- app/models/application_service_connector_configuration.rb
+++ app/models/application_service_connector_configuration.rb
@@ -14,18 +14,6 @@ class ApplicationServiceConnectorConfiguration < ActiveRecord::Base
     @existing_service_params ||= existing_service.present? ? JSON.parse(existing_service).deep_symbolize_keys : {}
   end
 
-  def service_handle
-    existing_service_params[:service_handle]
-  end
-
-  def service_container_type
-    existing_service_params[:container_type]
-  end
-
-  def service_container_name
-    existing_service_params[:service_handle]
-  end
-
   def variable_values_params
     {}.tap do |result|
       variables.each do |variable|
